251ea42718461cf787be2a1225608826842c3d4e
GeovannyRomero <89670826+GeovannyRomero@users.noreply.github.com>
2021-08-30 22:02:04 -0600

    Add files via upload

---
+++ Elementos basicos II/Arreglos/Arreglos/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Parentesco = new string[4];
+            Parentesco[0] = "Abuelo";
+            Parentesco[1] = "Mamà";
+            Parentesco[2] = "Abuela";
+            Parentesco[3] = "Hermana";
+
+            string[] nombres = new string[4];
+            Parentesco[0] = "Gonzalo";
+            Parentesco[1] = "Lorena";
+            Parentesco[2] = "Maria";
+            Parentesco[3] = "Abigail";
+
+            int[] edad = new int[4];
+            edad[0] = 56;
+            edad[1] = 35;
+            edad[2] = 56;
+            edad[3] = 10;
+
+            for (int i = 0; i < Parentesco.Length; i++)
+            {
+                Console.WriteLine(" Parentesco {0}: {1} ", i, Parentesco[i]);
+            }
+            for (int i = 0; i < Parentesco.Length; i++)
+            {
+                Console.WriteLine(" Parentesco {0}: {1} ", i, Parentesco[i]);
+            }
+            Array.Sort(edad);
+            for (int i = 0; i < edad.Length; i++)
+            {
+                Console.WriteLine(" edad {0}: {1} ", i, edad[i]);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Elementos basicos II/PracticaNº1/PracticaNº1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaNº1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //definir un diccionario
+            Dictionary<string, float> Nombres = new Dictionary<string, float>();
+            //agregar elementos al diccionario
+            Nombres.Add("Lorena", 36f);
+            Nombres.Add("Abigail", 10f);
+            Nombres.Add("Geovanny", 18f);
+
+            //leer diccionario y mostrar datos
+            foreach (KeyValuePair<string, float> Nm in Nombres)
+            {
+                Console.WriteLine("Key: {0}, Value: {1}", Nm.Key, Nm.Value);
+            }
+            //ordenar diccionario
+            //definir diccionario como una lista
+            var ordenar = Nombres.Keys.ToList();
+            ordenar.Sort();
+            Console.WriteLine("Diccionario ordenado");
+            foreach (var T in ordenar)
+            {
+                Console.WriteLine("Key: {0}, Value: {1}", T, Nombres[T]);
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
